a1eb7896bb91a3ac2627efdadbdfdcefd79ac7bb
Bradley-Laubscher <bradleylaubscher@gmail.com>
2025-03-27 19:00:01 +0200

    highlight selected location on map when location in listView is clicked.

--- lib/Widgets/Children/map.dart
+++ lib/Widgets/Children/map.dart
@@ -2,32 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:provider/provider.dart';
 import 'package:surfspot/Config/config.dart';
+import 'package:surfspot/Providers/location_provider.dart';
 
 class DestinationMap extends StatelessWidget {
   const DestinationMap({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final selectedLocation = Provider.of<LocationProvider>(context).selectedLocation;
+
     return ConstrainedBox(
       constraints: const BoxConstraints(
         minHeight: 250,
-        minWidth: 400
+        minWidth: 400,
       ),
       child: Container(
         margin: const EdgeInsets.only(top: 20),
-        width: MediaQuery.of(context).size.width*0.6,
-        height: MediaQuery.of(context).size.height*0.3,
+        width: MediaQuery.of(context).size.width * 0.6,
+        height: MediaQuery.of(context).size.height * 0.3,
         decoration: BoxDecoration(
           border: Border.all(
             color: Colors.black,
             width: 1,
-            style: BorderStyle.solid
-          )
+            style: BorderStyle.solid,
+          ),
         ),
         child: FlutterMap(
-          options: const MapOptions(
-            initialCenter: LatLng(-34.008856, 18.581152),
+          options: MapOptions(
+            initialCenter: LatLng(
+              double.parse(selectedLocation["latitude"]),
+              double.parse(selectedLocation["longitude"]),
+            ),
             initialZoom: 9.0,
           ),
           children: [
@@ -37,20 +44,26 @@ class DestinationMap extends StatelessWidget {
             ),
             MarkerLayer(
               markers: locations.map((spot) {
+                bool isSelected = spot["name"] == selectedLocation["name"];
+
                 return Marker(
                   point: LatLng(
                     double.parse(spot["latitude"]),
                     double.parse(spot["longitude"]),
                   ),
-                  width: 40,
-                  height: 40,
-                  child: const Icon(Icons.location_on, color: Colors.red, size: 30),
+                  width: isSelected ? 50 : 40, // Highlight selected marker
+                  height: isSelected ? 50 : 40,
+                  child: Icon(
+                    Icons.location_on,
+                    color: isSelected ? Colors.blue : Colors.red, // Blue for selected marker
+                    size: isSelected ? 40 : 30,
+                  ),
                 );
               }).toList(),
             ),
           ],
         ),
-      )
+      ),
     );
   }
 }
